14820f9e36843354eb676d772c2eeb6147ee6064
Bhavik <kapadiab@uoguelph.ca>
2025-10-24 11:44:18 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -2,24 +2,30 @@
 
 # Fibonacci Sequence Exercise with functions
 def positive_num():
-  while True:
-    try:
-      terms = int(input("Enter the number of terms:"))
-      if terms > 0:
-        return value
+    while True:
+        try:
+            terms = int(input("Enter the number of Fibonacci terms: "))
+            if terms > 0:
+                return terms
+            else:
+                print("Please enter a positive integer greater than zero.")
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+
 def fibonacci(n):
-  sequence = []
-  a, b = 0, 1
-  for i in range(n):
-    sequence.append(a)
-    a, b = b, a + b
-  return sequence
+    sequence = []
+    a, b = 0, 1
+    for _ in range(n):
+        sequence.append(a)
+        a, b = b, a + b
+    return sequence
+
 def print_sequence(sequence):
-  print("The fibonacci sequence is:", fib)
-  print(", ".join(str(num) for num in sequence))
-  print("Sum of this sequence is:", sum(sequence))
+    print("Fibonacci sequence:")
+    print(", ".join(str(num) for num in sequence))
+    print("Sum of the sequence:", sum(sequence))
 
 total_terms = positive_num()
 fib_sequence = fibonacci(total_terms)
-print_sequence(total_terms)
+print_sequence(fib_sequence)
 # TODO: (Read detailed instructions in the Readme file)
